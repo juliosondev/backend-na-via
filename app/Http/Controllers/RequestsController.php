a0a649bfe1fb3c8fb48e32dd2c5e952d28df6abb
Ricardo-Cambundo <ricardocmbd@gmail.com>
2024-09-17 21:47:52 +0100

    another commit for requests

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Pedido;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -48,4 +49,58 @@ class RequestsController extends Controller
 
     return response()->json($pedidos);
 }
+    public function availableRequests()
+    {
+        $pedidos = DB::table('pedidos')
+        ->whereNull('info->motoBoy')
+        ->leftJoin('users', 'pedidos.user_cliente_id', '=', 'users.id')
+        ->join('users_dados', 'users.id', '=', 'users_dados.user_id')
+        ->select('pedidos.*', 'users.*', 'users.id as client_id', 'users_dados.id as user_dados_id', 'users_dados.*', 'pedidos.id as id', 'pedidos.status as status')
+        ->get();
+
+
+
+
+
+        return response()->json($pedidos);
+    }
+    public function acceptedRequests($id)
+    {
+        $pedidos = DB::table('pedidos')
+        ->where('info->motoBoy->user_id', $id)
+        ->leftJoin('users', 'pedidos.user_cliente_id', '=', 'users.id')
+        ->join('users_dados', 'users.id', '=', 'users_dados.user_id')
+        ->select('pedidos.*', 'users.*', 'users.id as client_id', 'users_dados.id as user_dados_id', 'users_dados.*', 'pedidos.id as id', 'pedidos.status as status')
+        ->get();
+
+
+
+
+
+        return response()->json($pedidos);
+    }
+    public function request($id)
+    {
+        $pedido = Pedido::findOrFail($id);
+        return response()->json($pedido);
+    }
+
+    public function editRequest(Request $request, $id, $field)
+    {
+        $pedido = Pedido::findOrFail($id);
+        if ($field == 'cancel'){
+            $pedido->status = 'inactivo';
+            $pedido->info = $request->input('info');
+            $pedido->save();
+            return response()->json($request->all());
+        }else if ($field == 'accept'){
+            $pedido->info = $request->input('info');
+            $pedido->save();
+            return response()->json($request->all());
+        }else if ($field == 'stats') {
+            $pedido->info = $request->input('info');
+            $pedido->save();
+            return response()->json($pedido);
+        }
+    }
 }
